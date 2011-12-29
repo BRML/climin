56eae8743cb8c63b44e9e65769f62e65ce8e8e90
Justin Bayer <bayer.justin@googlemail.com>
2011-12-29 11:26:10 +0100

    Added Rosenbrock function to tests for bfgs and prop.

--- test/test_bfgs.py
+++ test/test_bfgs.py
@@ -1,4 +1,5 @@
 import scipy
+from scipy.optimize import rosen, rosen_der
 
 from climin import Bfgs 
 
@@ -8,7 +9,21 @@ quadraticprime = lambda x: 2 * x
 quadraticandprime = lambda x: (quadratic(x), quadraticprime(x))
 
 
-def test_bfgs():
+def test_bfgs_rosen():
+    dim = 2
+    wrt = scipy.zeros((dim,))
+    f = lambda: rosen(wrt)
+    fprime = lambda: rosen_der(wrt)
+
+    opt = Bfgs(wrt, f, fprime)
+    for i, info in enumerate(opt):
+        if i > 1000:
+            break
+    print wrt
+    assert (abs(wrt - [1, 1]) < 0.01).all(), 'did not find solution'
+
+
+def test_bfgs_quadratic():
     dim = 2
     wrt = scipy.random.standard_normal((dim,)) * 10 + 5
     f = lambda: quadratic(wrt)
@@ -16,6 +31,7 @@ def test_bfgs():
 
     opt = Bfgs(wrt, f, fprime)
     for i, info in enumerate(opt):
-        if i > 10:
+        if i > 100:
             break
+    print wrt
     assert (abs(wrt) < 0.01).all(), 'did not find solution'
--- test/test_rprop.py
+++ test/test_rprop.py
@@ -1,4 +1,5 @@
 import scipy
+from scipy.optimize import rosen, rosen_der
 
 from climin import Rprop
 
@@ -8,7 +9,7 @@ quadraticprime = lambda x: 2 * x
 quadraticandprime = lambda x: (quadratic(x), quadraticprime(x))
 
 
-def test_rprop():
+def test_rprop_quadratic():
     dim = 10
     wrt = scipy.random.standard_normal((dim,)) * 10 + 5
 
@@ -20,3 +21,17 @@ def test_rprop():
         if i > 1000:
             break
     assert (abs(wrt) < 0.01).all(), 'did not find solution'
+
+
+def test_rprop_rosen():
+    dim = 2
+    wrt = scipy.zeros((dim,))
+    fandprime = lambda: (rosen(wrt), rosen_der(wrt))
+
+    opt = Rprop(wrt, fandprime, stepshrink=0.5, stepgrow=1.1,
+            minstep=1E-10, maxstep=0.1)
+    for i, info in enumerate(opt):
+        if i > 10000:
+            break
+    print wrt
+    assert (abs(wrt - [1, 1]) < 0.01).all(), 'did not find solution'
