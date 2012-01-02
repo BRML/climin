32f2ad2d90c861daefd45c2fc4806135170b9760
Justin Bayer <bayer.justin@googlemail.com>
2012-01-02 12:40:59 +0100

    FIX: took away brackets from assert (otherwise always true)

--- test/test_linesearch.py
+++ test/test_linesearch.py
@@ -12,14 +12,14 @@ def test_polyinterp():
     points = np.array([[0, 13675.4102446153, -51949952485403.5],\
             [5.13158867008399e-10, 60596.6916564044,408884281904298]])
     t, fm = linesearch.polyinterp(points)
-    assert (abs(t - 1.94786036422603e-10) < 10**-12, 'polyinterp failed.')
+    assert abs(t - 1.94786036422603e-10) < 10**-12, 'polyinterp failed.'
     #
     points = np.array([[0, 3455.1645406324, -459.680191241405],\
             [1, 3459.20341983103,837.841741413213]])
     t, fm = linesearch.polyinterp(points)
-    assert (abs(t - 0.564620052389446) < 10**-12, 'polyinterp failed.')
+    assert abs(t - 0.564620052389446) < 10**-12, 'polyinterp failed.'
     #
     points = np.array([[0,5685.6017238964, -121.263963883048],\
             [1, 5569.21178006813, -111.62733563021]])
     t, fm = linesearch.polyinterp(points, xminBound=1.01, xmaxBound = 10)
-    assert (t==10, 'polyinterp failed.')
+    assert t==10, 'polyinterp failed.'
