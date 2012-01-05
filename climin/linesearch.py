FIX: line search would not save gradient

climin/linesearch.py
@@ -85,8 +85,8 @@ class StrongWolfeBackTrack(BackTrack):
                 dir_dot_grad = scipy.inner(direction, grad)
                 # Wolfe 2
                 if abs(dir_dot_grad) <= self.c2 * abs(dir_dot_grad0):
+                    self.grad = grad
                     return s
-
         return 0.0
 
 