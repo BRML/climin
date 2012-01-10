92ebd0ea6971e639f6d530dbd9be06a01fbf1d84
Justin Bayer <bayer.justin@googlemail.com>
2012-01-10 11:33:01 +0100

    Minor fixes and license information.

--- climin/nes.py
+++ climin/nes.py
@@ -1,5 +1,33 @@
 # -*- coding: utf-8 -*-
 
+# This code has been adapted from PyBrain. See http://pybrain.org.
+#
+# Copyright (c) 2009, PyBrain-Developers
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright notice,
+#       this list of conditions and the following disclaimer in the documentation
+#       and/or other materials provided with the distribution.
+#     * Neither the name of PyBrain nor the names of its contributors
+#       may be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 import scipy
 import scipy.linalg
@@ -38,14 +66,15 @@ class Xnes(Minimizer):
             samples = scipy.dot(samples, A) + center
             fitnesses = [self.f(samples[j], *args, **kwargs) 
                          for j in range(samples.shape[0])]
-            fitnesses = scipy.array(fitnesses)
+            fitnesses = scipy.array(fitnesses).flatten()
+
             if fitnesses.max() > best_x:
                 best_loss = fitnesses.max()
                 self.wrt[:] = samples[fitnesses.argmax()]
 
             # Update center and variances.
             utilities = self.compute_utilities(fitnesses)
-            center += scipy.dot(A, scipy.dot(utilities, samples))
+            center += scipy.dot(scipy.dot(utilities, samples), A)
             # TODO: vectorize this
             cov_gradient = sum([u * (scipy.outer(s, s) - I) 
                                 for (s, u) in zip(samples, utilities)])
