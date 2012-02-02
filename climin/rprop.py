a91041c8b0cf97c1a02addfface2a853c1647d96
Justin Bayer <bayer.justin@googlemail.com>
2012-02-02 09:41:02 +0100

    Made GradientDescent and Rprop use f, fprime instead of fandprime.

--- climin/rprop.py
+++ climin/rprop.py
@@ -9,12 +9,13 @@ from base import Minimizer, repeat_or_iter
 
 class Rprop(Minimizer):
 
-    def __init__(self, wrt, fandprime, stepshrink=0.5, stepgrow=1.2,
+    def __init__(self, wrt, f, fprime, stepshrink=0.5, stepgrow=1.2,
                  minstep=1E-6, maxstep=1, changes_max=0.1,
                  args=None, stop=1, logfunc=None):
         super(Rprop, self).__init__(wrt, args=args, stop=stop, logfunc=logfunc)
 
-        self.fandprime = fandprime
+        self.f = f
+        self.fprime = fprime
         self.stepshrink = stepshrink
         self.stepgrow = stepgrow
         self.minstep = minstep
@@ -26,7 +27,7 @@ class Rprop(Minimizer):
         changes = scipy.random.random(self.wrt.shape) * self.changes_max
 
         for i, (args, kwargs) in enumerate(self.args):
-            loss, grad = self.fandprime(self.wrt, *args, **kwargs)
+            grad = self.fprime(self.wrt, *args, **kwargs)
             changes_min = changes * self.stepgrow
             changes_max = changes * self.stepshrink
             gradprod = grad_m1 * grad
@@ -47,5 +48,6 @@ class Rprop(Minimizer):
             # TODO: yield correct loss
             # TODO: log something
             if i > 0 and i % self.stop == 0:
+                loss = self.f(self.wrt, *args, **kwargs)
                 yield dict(loss=loss, args=args, kwargs=kwargs, grad=grad,
                            step=step)
--- test/test_gd.py
+++ test/test_gd.py
@@ -5,14 +5,13 @@ from climin import GradientDescent
 
 quadratic = lambda x: (x**2).sum()
 quadraticprime = lambda x: 2 * x
-quadraticandprime = lambda x: (quadratic(x), quadraticprime(x))
 
 
 def test_gradient_descent_constant_step():
     dim = 10
     wrt = scipy.random.standard_normal((dim,)) * 10 + 5
 
-    opt = GradientDescent(wrt, quadraticandprime, steprate=0.01)
+    opt = GradientDescent(wrt, quadratic, quadraticprime, steprate=0.01)
     for i, info in enumerate(opt):
         if i > 1000:
             break
