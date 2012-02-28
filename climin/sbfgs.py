1f628ab5050ec91506252044a07b0fac4095407b
Justin Bayer <bayer.justin@googlemail.com>
2012-02-28 13:34:13 +0100

    removed convergence criterion

--- climin/sbfgs.py
+++ climin/sbfgs.py
@@ -37,13 +37,6 @@ class SBfgs(Minimizer):
             self.inv_hessian = scipy.eye(grad.shape[0])
 
         for i, (next_args, next_kwargs) in enumerate(self.args):
-            # If the gradient is exactly zero, we stop. Otherwise, the
-            # updates will lead to NaN errors because the direction will
-            # be zero.
-            if (grad == 0.0).all():
-                self.logfunc({'message': 'converged - gradient is 0'})
-                break
-
             if i == 0:
                 direction = -grad
             else:
@@ -68,12 +61,17 @@ class SBfgs(Minimizer):
                 H += np.outer(step, step) / ys
                 direction = - np.dot(H, grad)
 
-            steplength = self.line_search.search(direction, None, args, kwargs)
-            if steplength == 0:
-                self.logfunc({'message': 'converged - steplength is 0'})
+            if (direction == 0.0).all():
+                self.logfunc({'message': 'direction is 0 -- need to bail out.'})
                 break
-            step = steplength * direction
-            self.wrt += step
+
+            steplength = self.line_search.search(direction, None, args, kwargs)
+
+            if steplength != 0:
+                step = steplength * direction
+                self.wrt += step
+            else:
+                self.logfunc({'message': 'step length is 0.'})
 
             # Prepare everything for the next loop.
             args, kwargs = next_args, next_kwargs
