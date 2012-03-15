754e4c6a69d90e3145b8d6863378ccf40016bf14
Justin Bayer <bayer.justin@googlemail.com>
2012-03-15 13:30:22 +0100

    added module for helpers with initialization of parameters, containing a function to sparsify columns of a matrix

---
+++ test/test_initialize.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+
+from climin.initialize import sparsify_columns
+
+
+def test_sparsify_columns():
+    pars = np.ones((8, 10))
+    sparsify_columns(pars, 3)
+    assert (pars.sum(axis=0) == [3] * 10).all(), 'sparsify_columns did not work'
