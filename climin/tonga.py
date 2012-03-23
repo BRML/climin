394b6cccfcd30e88f47d2539f3ae5b945b3a4e2e
SDG <sdg@jlbl.net>
2012-03-23 13:51:25 +0100

    working version of tonga for tests

--- climin/tonga.py
+++ climin/tonga.py
@@ -2,30 +2,34 @@
 
 import scipy
 import numpy as np
-import numpy.linalg
+import scipy.linalg
 import scipy.optimize
 import itertools
 import math
+import scipy.sparse.linalg
 
 from base import Minimizer, repeat_or_iter
 
 
 class tonga(Minimizer):
 
-    def __init__(self, wrt, fgrad, fjacobian, damping, blocksizes,
-                 gamma=0.995, b=50, k=5,
-                 args=None, logfunc=None):
+    def __init__(self, wrt, fprime, damping, blocksizes,
+                 gamma=0.995, b=50, k=5, nb_estimates=50, 
+                 args=None, cov_args=None, logfunc=None):
+        
         super(tonga, self).__init__(
             wrt, args=args, logfunc=logfunc)
 
-        self.fgrad = fgrad
-        self.fjacobian = fjacobian
+        self.cov_args = cov_args if cov_args is not None else self.args
+
+        self.fprime = fprime
         self.damping = damping
         self.blocksizes = blocksizes
         self.gamma = gamma
         self.gamma_sqrt = math.sqrt(self.gamma)
         self.b = b #TODO find a meaningful name
         self.k = k
+        self.nb_estimates = nb_estimates
 
           
 
@@ -33,42 +37,45 @@ class tonga(Minimizer):
     def __iter__(self):
         X_m1 = np.zeros((1, self.wrt.size))
         oldGrad = np.zeros((self.b -1, self.wrt.size))
-        step = scipy.empty(self.wrt.size)
-
-        args, kwargs = self.args.next()
+        step = scipy.empty(self.wrt.size)        
 
         
-        for i, (next_args, next_kwargs) in enumerate(self.args):            
+        for i, (args, kwargs) in enumerate(self.args):            
             offset = 0
             if (i==0):
-                gradient_mean = self.fgrad(self.wrt, *args, **kwargs)
+                gradient_mean = self.fprime(self.wrt, *args, **kwargs)
                 X = scipy.empty((self.wrt.size,1)) 
                 X[:,0] = gradient_mean
             elif ((i%self.b)!=0):
-                gradient_mean = self.fgrad(self.wrt, *args, **kwargs)
+                gradient_mean = self.fprime(self.wrt, *args, **kwargs)
                 X = scipy.empty((self.wrt.size, X_m1[0].size + 1))
                 X[:,:-1] = X_m1 * self.gamma_sqrt
                 X[:,-1] = gradient_mean
             else:
-                gradient = self.fjacobian(self.wrt, *args, **kwargs)
+                #gradient = self.fjacobian(self.wrt, *args, **kwargs)
+                #gradient_mean = gradient.mean(axis=0)
+                batches = [self.cov_args.next() for _ in range(self.nb_estimates)]
+                gradient = [self.fprime(self.wrt, *cov_args, **cov_kwargs) for (cov_args, cov_kwargs) in batches]
+                gradient = np.array(gradient)
                 gradient_mean = gradient.mean(axis=0)
+                
                 X = scipy.empty((self.wrt.size, self.k + self.b)) 
 
                 
             for size in self.blocksizes:
                 if (i%self.b==0) and (i>0):
-
+                    #computing gradients
+                    grad = gradient[:, offset:offset+size]                    
+                    
+                    # using old gradients to estimate the new X
                     factor = [self.gamma_sqrt**power for power in range(self.b-1,0,-1)]
                     X[offset:offset+size, self.k:self.k+self.b-1] = factor * oldGrad.T[offset:offset+size]
                     
                     X[offset:offset+size, self.k+self.b-1] = self.gamma_sqrt**(self.b+i) * gradient_mean[offset:offset+size]
-                    
-                    grad = gradient[:, offset:offset+size]
+
+                    #eigenvalues decomposition                    
                     covariance = scipy.dot(grad.T, grad)
-                    V, U = np.linalg.eig(covariance)
-                    ind = V.argsort()
-                    X[offset:offset+size, :self.k] = self.gamma * (U[:, ind])[:, -self.k:]
-                                                                 
+                    _,  X[offset:offset+size, :self.k] = scipy.sparse.linalg.eigsh(covariance, k=self.k)                                                               
 
                 x = X[offset:offset+size]
                 step[offset:offset+size] = scipy.dot(x, scipy.linalg.inv(scipy.dot(x.T,x)+ self.damping*scipy.eye(len(x[0]))))[:,-1]
@@ -94,6 +101,5 @@ class tonga(Minimizer):
                 'step':step,
             }
         
-            args, kwargs = next_args, next_kwargs
 
             
--- examples/tongaMNIST.py
+++ examples/tongaMNIST.py
@@ -1,9 +1,9 @@
 import itertools
 import random
+import time
 
 from climin import tonga, GradientDescent
 
-import pylab
 import scipy
 import theano
 import theano.tensor as T
@@ -66,7 +66,7 @@ output_in = exprs['output-in']
 # Shorthand to create a cross entropy expression
 def cross_entropy(a, b):
     epsilon = 0
-    return -(a * T.log(b+epsilon)).sum(axis=1)
+    return -(a * T.log(b+epsilon)).mean()
 
 # Vector for the expression of the Hessian vector product, where this will
 # be the vector.
@@ -75,24 +75,24 @@ p = T.vector('p')
 
 # The loss and its gradient.
 loss = cross_entropy(target, output)
-lossmean = T.mean(loss)
-lossgrad = T.grad(lossmean, P.flat)
-lossjacobian = theano.gradient.jacobian(loss, P.flat)
+lossgrad = T.grad(loss, P.flat)
+misclassifications = 100 * T.mean(T.abs_(target-output))
 
 # Functions.
 givens = {P.flat: par_sub}
-f = theano.function([par_sub, inpt, target], lossmean, givens=givens)
-fraw = theano.function([par_sub, inpt, target], loss, givens=givens)
-fjacobian = theano.function([par_sub, inpt, target], lossjacobian, givens=givens)
-fgrad = theano.function([par_sub, inpt, target], lossgrad, givens=givens)
+f = theano.function([par_sub, inpt, target], loss, givens=givens)
+fprime = theano.function([par_sub, inpt, target], lossgrad, givens=givens)
+fmisclass = theano.function([par_sub, inpt, target], misclassifications, givens=givens)
 
 # Build a dataset.
 #MNIST
 import cPickle, gzip
 
 # Load the dataset
-with gzip.open('mnist.pkl.gz','rb') as MNISTfile:
-    train_set, valid_set, test_set = cPickle.load(MNISTfile)
+MNISTfile = gzip.open('mnist.pkl.gz','rb') 
+train_set, valid_set, test_set = cPickle.load(MNISTfile)
+MNISTfile.close()
+
 
 X, labels = train_set
 N, _ = X.shape
@@ -100,13 +100,16 @@ Y = np.zeros((N, 10))
 for i in range(N):
     Y[i][labels[i]] = 1
 
-print "data loaded"
-#initialization
-P['hiddenbias'][:] = np.random.randn(n_hidden)*1e-4
-P['outbias'][:] = np.random.randn(n_output)*1e-4
 
-P['inweights'][:,:] = np.random.randn(n_inpt, n_hidden)*1e-4
-P['outweights'][:,:] = np.random.randn(n_hidden, n_output)*1e-4
+print "data loaded"
+
+
+#initialization
+P['hiddenbias'][:] = np.random.randn(n_hidden)
+P['outbias'][:] = np.random.randn(n_output)
+
+P['inweights'][:,:] = np.random.randn(n_inpt, n_hidden)
+P['outweights'][:,:] = np.random.randn(n_hidden, n_output)
 
 
 
@@ -118,6 +121,13 @@ random_numbers = (random.randint(0, n_batches - 1) for _ in itertools.count())
 idxs = ((r * batchsize, (r + 1) * batchsize) for r in random_numbers)
 minibatches = ((X[lower:upper], Y[lower:upper]) for lower, upper in idxs)
 args = ((m, {}) for m in minibatches)
+
+cov_batchsize = 10
+cov_n_batches = N/cov_batchsize
+cov_random_numbers = (random.randint(0, cov_n_batches - 1) for _ in itertools.count())
+cov_idxs = ((r * cov_batchsize, (r + 1) * cov_batchsize) for r in cov_random_numbers)
+cov_minibatches = ((X[lower:upper], Y[lower:upper]) for lower, upper in cov_idxs)
+cov_args = ((m, {}) for m in cov_minibatches)
 
 print '#pars:', P.data.size
 
@@ -147,13 +157,18 @@ blocksizes = np.ones(n_hidden + n_output)
 blocksizes[:n_hidden] *= (n_inpt+1)
 blocksizes[n_hidden:] *= (n_hidden+1)
 
-opt = tonga(P.data, fgrad, fjacobian
-            , damping=1e-4, blocksizes=blocksizes, args=args, logfunc=logfunc)
-#opt = GradientDescent(P.data, fgrad, steprate = 1e-4, args=args, logfunc=logfunc)
+opt = tonga(P.data, fprime, damping=1e-4, blocksizes=blocksizes, args=args, cov_args=cov_args, logfunc=logfunc)
+#opt = GradientDescent(P.data, fprime, steprate = 1e-2, momentum=0.9, args=args, logfunc=logfunc)
 
 print "initialization done"
 
-lossTab = scipy.empty(503)
+N_ITER_MAX = 500
+
+lossTab = scipy.empty(N_ITER_MAX +3)
+scheduleTab = scipy.empty(N_ITER_MAX +3)
+lossTot =  scipy.empty(N_ITER_MAX/50 +1)
+miscclassif = scipy.empty(N_ITER_MAX/50 +1)
+start = time.clock()
 
 for i, info in enumerate(opt):
     x, y = info['args']
@@ -161,21 +176,39 @@ for i, info in enumerate(opt):
     print 'iteration', i
     print 'loss', loss
     lossTab[i] = loss
-    
-    
+    scheduleTab[i] = time.clock()-start
+
+    if i%50 == 0:
+        delay = time.clock()
+        loss = f(P.data, *(X,Y), **{})
+        print '---'
+        print 'loss on the whole dataset', loss
+        lossTot[i/50] = loss
+        miscclassif[i/50] = fmisclass(P.data, *(X,Y), **{})
+        print 'misclassification: ', miscclassif[i/50], '%'
+        print '---'
+        start += (time.clock()-delay)
     logfunc(info)
-    if i > 500:
+    if i >= N_ITER_MAX:
         break
 
-fileLog = open('logTonga', 'w')
+fileName = 'logTonga'
+#fileName = 'logSgd'
+fileLog = open(fileName, 'w')
 fileLog.write('')
 fileLog.close()
-fileLog = open('logTonga', 'a')
-for i in range(501):
+fileLog = open(fileName, 'a')
+for i in range(N_ITER_MAX+1):
     fileLog.write(str(lossTab[i]))
     fileLog.write('\n')
+    fileLog.write(str(scheduleTab[i]))
+    fileLog.write('\n')
+for i in range(N_ITER_MAX/50):
+    fileLog.write(str(lossTot[i]))
+    fileLog.write('\n')
+    fileLog.write(str(miscclassif[i]))
+    fileLog.write('\n')   
 fileLog.close()
 
-pylab.plot(lossTab)
-pylab.show()
+
 
