c657622a3418f68f7fdf2995e22b7712f2a72206
Justin Bayer <bayer.justin@googlemail.com>
2012-03-23 08:58:09 +0100

    pep8

--- climin/stops/__init__.py
+++ climin/stops/__init__.py
@@ -1,4 +1,3 @@
-from stops import after_1000_iterations
 from stops import after_n_iterations
 from stops import modulo_n_iterations
 from stops import time_elapsed
--- climin/stops/stops.py
+++ climin/stops/stops.py
@@ -1,34 +1,33 @@
-import time
+# -*- coding: utf-8 -*-
 
-def after_1000_iterations(info):
-    """ this is a very simple example stop criterion that stops 
-        after 1000 iterations. stop criterions need to be callables 
-        that take the info dictionary and return either True or False.
-    """
-    return info['n_iter'] >= 1000-1
+
+import time
 
 # Stop criterions can be simple functions like the above one, but
 # most of the following functions are actually stop criterion
 # factories, that return a stop criterion upon initialization.
 
+
 def after_n_iterations(n):
-    """ returns a stop criterion that stops after n iterations. """
+    """Return a stop criterion that stops after `n` iterations. """
     def inner(info):
-        return info['n_iter'] >= (n-1)
+        return info['n_iter'] >= n - 1
     return inner
-    
-    
+
+
 def modulo_n_iterations(n):
-    """ returns a stop criterion that stops at each n-th iteration. 
-        Example for n=5: stops at n_iter = 0, 5, 10, 15, ...
+    """Return a stop criterion that stops at each `n`-th iteration.
+
+    E.g.  for n=5, stops at n_iter = 0, 5, 10, 15, ...
     """
     def inner(info):
         return info['n_iter'] % n == 0
     return inner
-    
+
 
 def time_elapsed(sec):
-    """ returns a stop criterion that stops after `sec` seconds after initializing. """
+    """Return a stop criterion that stops after `sec` seconds after
+    initializing."""
     start = time.time()
     def inner(info):
         return time.time() - start > sec
@@ -36,31 +35,31 @@ def time_elapsed(sec):
 
 
 def converged(func, n=10, epsilon=1e-5):
-    """ returns a stop criterion that remembers the last n return values of func() in 
-        a buffer and stops if the difference of max(buffer) - min(buffer) is smaller 
-        than epsilon. `func` needs to be a callable that returns a scalar value. 
+    """Return a stop criterion that remembers the last `n` return values of
+    `func`() and stops if the difference of their maximum and their minimum is
+    smaller than `epsilon`.
+
+    `func` needs to be a callable that returns a scalar value.
     """
     ringbuffer = [None for i in xrange(n)]
     def inner(info):
         ringbuffer.append(func())
         ringbuffer.pop(0)
         if not None in ringbuffer:
-            ret = (max(ringbuffer) - min(ringbuffer)) < epsilon
+            ret = max(ringbuffer) - min(ringbuffer) < epsilon
         else:
-            ret = False 
+            ret = False
 
         return ret
     return inner
 
 
 def and_(criterions):
-    """ takes a list of stop criterions and returns a stop criterion that
-        only returns True, if all of criterions return True. This basically
-        implements a logical AND for stop criterions. 
+    """Return a stop criterion that given a list `criterions` of stop criterions
+    only returns True, if all of criterions return True.
+
+    This basically implements a logical AND for stop criterions.
     """
     def inner(info):
         return all(c(info) for c in criterions)
     return inner
-
-
-
