a142ad1d0df202047b63c5ca24e7ed1969302489
Justin Bayer <bayer.justin@googlemail.com>
2012-08-09 14:09:37 +0200

    Added convenience function for building optimizers.

--- climin/util.py
+++ climin/util.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
 
+import inspect
 import random
+
+from asgd import Asgd
+from gd import GradientDescent
+from ksd import KrylovSubspaceDescent
+from lbfgs import Lbfgs
+from ncg import NonlinearConjugateGradient
+from rprop import Rprop
+from smd import Smd
 
 
 def coroutine(f):
@@ -25,7 +34,7 @@ def mini_slices(n_samples, batch_size):
     n_batches, rest = divmod(n_samples, batch_size)
     if rest != 0:
         n_batches += 1
-        
+
     return [slice(i * batch_size, (i + 1) * batch_size) for i in range(n_batches)]
 
 
@@ -40,3 +49,43 @@ def draw_mini_slices(n_samples, batch_size, with_replacement=False):
             random.shuffle(idxs)
             for i in idxs:
                 yield slices[i]
+
+
+def optimizer(identifier, wrt, *args, **kwargs):
+    """Return an optimizer with the desired configuration.
+
+    This is a convenience function if one wants to try out different optimizers
+    but wants to change as little code as possible.
+
+    Additional arguments and keyword arguments will be passed to the constructor
+    of the class. If the found class does not take the arguments supplied, this
+    will `not` throw an error, but pass silently.
+
+    :param identifier: String identifying the optimizer to use. Can be either
+        ``asgd``, ``ksd``, ``gd``, ``lbfgs``, ``ncg``, ``rprop`` or  ``smd``.
+    :param wrt: Numpy array pointing to the data to optimize.
+    """
+    klass_map = {
+        'asgd': Asgd,
+        'ksd': KrylovSubspaceDescent,
+        'gd': GradientDescent,
+        'lbfgs': Lbfgs,
+        'ncg': NonlinearConjugateGradient,
+        'rprop': Rprop,
+        'smd': Smd,
+        }
+    # Find out which arguments to pass on.
+    klass = klass_map[identifier]
+    argspec = inspect.getargspec(klass.__init__)
+    if argspec.keywords is None:
+        # We need to filter stuff out.
+        kwargs = dict((k, v) for k, v in kwargs.items()
+                      if k in argspec.args)
+
+    try:
+        opt = klass(wrt, *args, **kwargs)
+    except TypeError:
+        raise TypeError('required arguments for %s: %s' % (klass, argspec.args))
+
+    return opt
+
