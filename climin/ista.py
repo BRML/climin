8c44b24260a2366be4c941b94461b2f5d5140262
Justin Bayer <bayer.justin@googlemail.com>
2012-12-13 13:14:19 +0100

    added documentation

--- climin/ista.py
+++ climin/ista.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+"""This module provides functionality for iterative soft-thresholding,
+as described in
+
+    Machine learning: a probabilistic perspective
+    Kevin Murphy (2012).
+"""
+
 
 import itertools
 
@@ -9,14 +16,58 @@ from base import Minimizer
 
 
 def soft(a, b):
+    """Return `a` soft thresholded by `b`.
+
+    The mathematical formulation is
+    :math:`soft(a, b) = \\text{sign}(a)\max(|a| - b, 0)`.
+    """
     return np.sign(a) * np.maximum((abs(a) - b), 0)
 
 
 class Ista(Minimizer):
+    """Ista is a gradient descent based minimizer of a generic loss respecting
+    an additional l1 regularizer (or Laplace prior) on the parameters of the
+    solution."""
 
     def __init__(self, wrt, f_loss, f_residual, f_prime, c_l1, step_rate,
                  max_fraction=0.01, max_bad_steps=5,
                  min_step_rate=1e-8, max_step_rate=50, args=None):
+        """Create an Ista object.
+
+        Ista works by picking a local regularization scalar and then adapting
+        it. to a certain rule. As soon as the regularization scalar given by the
+        user is reached, optimization stops.
+
+        :param wrt: Numpy parameter array to optimize.
+        :param f_loss: Function returning the loss (without the regularization
+            term). The signature is `f(wrt, X, Z) = l` where `wrt` are the
+            current parameters, `X` the inputs and `Z` the targets and `l` a
+            scalar.
+        :param f_residual: Function returning the residuals of the current
+            parameters; in most cases, this will be the difference of the
+            prediction and the targets. The signature is `f(wrt, X, Z) = r`
+            where `wrt` are the current parameters, `X` the inputs, `Z` the
+            targets and `r` a numpy array of the same shape as Z containing the
+            residuals.
+        :param f_prime: Function returning the derivative of the current
+            parameters wrt to the parameters.
+            The signature is `f(wrt, X, Z) = g`
+            where `wrt` are the current parameters, `X` the inputs, `Z` the
+            targets and `g` a numpy array of the same shape as `wrt`.
+        :param c_l1: The coefficient for the l1 regularization, commonly
+            referred to as lambda.
+        :param step_rate: The initial step rate.
+        :param max_fraction: A constant for picking the next local c_l1.
+            The higher the constant, earlier it stops.
+        :param max_bad_steps: Maximum number of bad steps the algorithm takes
+            before moving on to the next c_l1. Bad steps are those which
+            increase the loss.
+        :param min_step_rate: Minimum step rate.
+        :param max_step_rate: Maximum step rate.
+        :param args: Iterator over the arguments for the loss. E.g. an infinite
+            iterator over `(X, Z), {}` where `X` is the input and `Z` the target
+            data.
+        """
         super(Ista, self).__init__(wrt, args=args)
 
         # For now, it only works with linear least squares. But that will
