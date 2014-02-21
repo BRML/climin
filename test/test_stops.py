4d9cb13ffec909f20738f1dda73ef52bea9cab72
Justin Bayer <bayer.justin@googlemail.com>
2014-02-21 15:02:11 +0100

    fixed test for patience

--- test/test_stops.py
+++ test/test_stops.py
@@ -20,21 +20,20 @@ def test_patience_constant():
     func = iter([10, 10, 10, 10]).next
     stopper = patience(func, 3, 2)
 
-    assert not stopper(None)
-    assert not stopper(None)
-    assert not stopper(None)
-    assert stopper(None), 'initial patience should be over'
+    assert not stopper({'n_iter': 0})
+    assert not stopper({'n_iter': 1})
+    assert not stopper({'n_iter': 2})
+    assert stopper({'n_iter': 3}), 'initial patience should be over'
 
 
 def test_patience_increase():
     func = iter([10, 10, 10, 5, 10, 10, 10]).next
     stopper = patience(func, 3, 2)
 
-    assert not stopper(None)
-    assert not stopper(None)
-    assert not stopper(None)
-    assert not stopper(None)
-    assert not stopper(None)
-    assert not stopper(None)
-    assert stopper(None)
-
+    assert not stopper({'n_iter': 0})
+    assert not stopper({'n_iter': 1})
+    assert not stopper({'n_iter': 2})
+    assert not stopper({'n_iter': 3})
+    assert not stopper({'n_iter': 4})
+    assert not stopper({'n_iter': 5})
+    assert stopper({'n_iter': 6})
